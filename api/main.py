a4864b51d509c06984c33364f486cb8d8a920683
Geon Kim <geon6757@kaist.ac.kr>
2023-01-26 16:03:57 +0900

    Update transformer version

--- api/main.py
+++ api/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Union
+from typing import List, Union
 from vit import ViT
 
 app = FastAPI()
 vit = ViT()
 
 class EmbeddingBody(BaseModel):
-    imageUrls: list[str]
+    imageUrls: List[str]
 
 @app.post("/embedding")
 def embeddingHandler(body: EmbeddingBody):
     try:
         return {
-            "vector": vit.embeddingFromUrl[body.imageUrls[0]]
+            "vector": vit.embeddingFromUrl(body.imageUrls[0])
         }
     except Exception as e:
         raise HTTPException(status_code=400, detail="error")
-
